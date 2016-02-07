fa57edc725b07cfff2ddcbdfac946886b75e1950
Joseph Elliott <joey.w.elliott@gmail.com>
2016-02-07 13:38:58 -0600

    Added Mongoose

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose = require('mongoose');
+
 var app = express();
+mongoose.connect('mongodb://55.55.55.5/mongo');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
