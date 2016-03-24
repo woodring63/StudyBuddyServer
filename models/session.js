53b199dd3cd938e0cda46352bb1f87a460c22836
Erica Clark <enclark@iastate.edu>
2016-03-23 22:52:50 -0500

    Modified the Session schema for geocoords prep and removed name

--- models/session.js
+++ models/session.js
@@ -2,17 +2,21 @@ var mongoose = require('mongoose');
 
 /**
  * The schema of the session. The SessionSchema object on the android side should format everything
- * accordingly.
+ * accordingly. Loc is basically a GeoJSON object and the loc type describes the type of coords given.
+ * Point is a good example.  For more info : https://docs.mongodb.org/manual/applications/geospatial-indexes/
  */
 
 var sessions = mongoose.Schema({
-	title: String,
 	startTime: Number,
 	endTime: Number,
     course: String,
 	attendees: Array,
 	messages: Array,
-	leader: String
+	leader: String,
+	loc: {
+		type: { type: String },
+		coordinates: []
+	}
 });
 
 module.exports = mongoose.model('Session', sessions);
