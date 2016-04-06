18bcb8f7c50f3256e5c4894dee4365f9a74ec25d
Erica Clark <enclark@iastate.edu>
2016-04-06 01:34:04 -0500

    New Module(Hopefully that didn't screw anything up) and basic socket.io

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+console.log(server);
 
 //Not used, but without it, the code breaks
 
@@ -7,5 +11,15 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
 	res.render('index', { title: 'Study Buddy' });
 });
+
+
+
+io.on('connection', function(socket){
+    socket.on('new message',function(msg){
+        console.log("message: " + msg);
+        io.emit('new message', {msg: msg});
+    });
+});
+server.listen(8000);
 
 module.exports = router;
